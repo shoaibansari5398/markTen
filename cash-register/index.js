b93a619c4e44cde8ecf9068458d590080d1e92f7
Shoaib Akhtar Ansari <shoaib.ansari5398@gmail.com>
2022-02-26 13:08:12 +0530

    Add files via upload

---
+++ cash-register/index.js
@@ -0,0 +1,38 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+checkButton.addEventListener("click", function calculateAmount() {
+  hideMessage();
+  if (billAmount.value > 0) {
+    if (cashGiven.value > billAmount.value) {
+      const amountToReturned = cashGiven.value - billAmount.value;
+      calculateNotes(amountToReturned);
+    } else {
+      showMessage("The cash provided should atleast be equal to bill amount");
+    }
+  } else {
+    showMessage("Invalid Bill Amount");
+  }
+});
+
+function hideMessage() {
+  message.style.display = "none";
+}
+
+function showMessage(msg) {
+  message.style.display = "block";
+  message.innerText = msg;
+}
+
+function calculateNotes(amountToReturned) {
+  for (let i = 0; i < availableNotes.length; i++) {
+    const numberOfNotes = Math.trunc(amountToReturned / availableNotes[i]);
+    amountToReturned = amountToReturned % availableNotes[i];
+    noOfNotes[i].innerText = numberOfNotes;
+  }
+}
